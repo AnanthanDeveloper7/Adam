867090f6c8ffcef5e71a4f8aa723366cecbd2a2d
HARI <HARI@DESKTOP-TGPGN4M>
2022-07-23 17:43:01 +0530

    First Commit

---
+++ $19/Strr19.java
@@ -0,0 +1,11 @@
+
+public class Strr19 {
+	public static void main (String [] args)
+	{
+		String s1 = String("hello");
+		String s2 = String("hello");
+		System.out.println(s1==s2);
+		System.out.println(s1.equals(s2));
+	}
+
+}
---
+++ $19/src/$19/Str1.java
@@ -0,0 +1,11 @@
+package $19;
+
+public class Str1 {
+	public static void main (String [] args) {
+		String s1 =("Hello");
+		String s2 = ("Hello");
+		System.out.println(s1 == s2);
+		System.out.println(s1.equals(s2));
+	}
+
+}
